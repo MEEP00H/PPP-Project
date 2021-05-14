26fc7dfe9a36356ac7bc07546d35ef74f8461459
Pooh <sophon@punchup.world>
2021-05-15 01:09:28 +0700

    comment loading screen

--- src/containers/main.js
+++ src/containers/main.js
@@ -9,85 +9,80 @@ import Contract from '../components/contract/contract';
 import NavigationBar from '../components/navigationBar/navigationBar';
 import TimeLine from '../components/timeLine/timeLine';
 import MyGallery from '../components/myGallery/myGallery';
-import Loading from '../components/UI/loading/loading';
+// import Loading from '../components/UI/loading/loading';
 
 export default class main extends Component {
+  state = {
+    intro: '',
+    profileImg: '',
+    name: '',
+    position: '',
+    skills: '',
+    contract: '',
+    experience: '',
+    error: false,
+  };
 
-    state ={
-        intro : '',
-        profileImg : '',
-        name : '',
-        position : '',
-        skills:'',
-        contract: '',
-        experience: '',
-        error:false
+  async componentDidMount() {
+    await axios.get('/intro.json').then((res) => {
+      this.setState({ intro: res.data });
+    });
+    await axios.get('/image-profile.json').then((res) => {
+      this.setState({ profileImg: res.data });
+    });
+    await axios.get('/name.json').then((res) => {
+      this.setState({ name: res.data });
+    });
+    await axios.get('/position.json').then((res) => {
+      this.setState({ position: res.data });
+    });
+    await axios.get('/skills.json').then((res) => {
+      this.setState({ skills: res.data });
+    });
+    await axios.get('/contract.json').then((res) => {
+      this.setState({ contract: res.data });
+    });
+    await axios
+      .get('/experience.json')
+      .then((res) => {
+        this.setState({ experience: res.data });
+      })
+      .catch((error) => {
+        this.setState({ error: true });
+      });
+  }
+
+  render() {
+    let page = this.state.error ? <p>can't be loaded</p> : <div></div>;
+
+    if (this.state.experience) {
+      page = (
+        <div className="main" id="main">
+          <NavigationBar />
+          <Intro />
+          <CoverLetter cover={this.state.intro} />
+          <Profile
+            profileImg={this.state.profileImg}
+            name={this.state.name}
+            position={this.state.position}
+          />
+          <Programming pgSkills={this.state.skills.programmings} />
+          <TimeLine experience={this.state.experience} />
+          <MyGallery myGallery={this.state.contract.gallery} />
+          <Contract contract={this.state.contract} />
+        </div>
+      );
     }
 
-    async componentDidMount(){
-        await axios.get('/intro.json')
-            .then(res =>{
-                this.setState({ intro:res.data })
-            })
-        await axios.get('/image-profile.json')
-            .then(res =>{
-                this.setState({ profileImg:res.data })
-            })
-        await axios.get('/name.json')
-            .then(res =>{
-                this.setState({ name:res.data })
-            })
-        await axios.get('/position.json')
-            .then(res =>{
-                this.setState({ position:res.data })
-            })
-        await axios.get('/skills.json')
-            .then(res =>{
-                this.setState({ skills:res.data })
-            })
-        await axios.get('/contract.json')
-            .then(res =>{
-                this.setState({ contract:res.data })
-            })
-        await axios.get('/experience.json')
-            .then(res =>{
-                this.setState({ experience:res.data }) 
-            }).catch(error => {
-                this.setState({error:true})
-            })
-    }   
-
-    render() {
-        let page = this.state.error ? <p>can't be loaded</p> : <Loading/>;
-
-        if(this.state.experience){
-            page = (
-                <div className = 'main' id = 'main'>
-                    <NavigationBar/>
-                    <Intro/>
-                    <CoverLetter cover = {this.state.intro}/>
-                    <Profile profileImg = {this.state.profileImg}
-                            name = {this.state.name}
-                            position = {this.state.position}
-                    />
-                    <Programming pgSkills = {this.state.skills.programmings}/>
-                    <TimeLine experience = {this.state.experience}/>
-                    <MyGallery myGallery = {this.state.contract.gallery}/>
-                    <Contract contract = {this.state.contract}/>
-                </div>
-            )
-        }
-
-        setTimeout(function() {
-            document.getElementById('load').style.display = 'none';
-            document.getElementById('main').style.display = 'block';
-        }, 9500);
-        return (
-            <div>
-                <Loading/>
-                {page}
-            </div>
-        )
-    }
+    // setTimeout(function () {
+    //   document.getElementById('load').style.display = 'none';
+    //   document.getElementById('main').style.display = 'block';
+    // }, 9500);
+    return (
+      <div>
+        {/* <Loading /> */}
+        {page}
+      </div>
+    );
+  }
 }
-
